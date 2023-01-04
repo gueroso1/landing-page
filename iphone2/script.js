0598946532e754b246c19e38a4a84ebbd5177d3b
Luiz Paulo Terra Nogueira <119353957+gueroso1@users.noreply.github.com>
2023-01-03 21:58:27 -0300

    site responsivo

---
+++ iphone2/script.js
@@ -0,0 +1,27 @@
+// função pra mudar o celular
+function imgSlider(e) {
+    document.querySelector('#phone').src = e
+}
+
+//função pra mudar a cor do circulo
+function circleChange(color) {
+    const circle = document.querySelector('#circle');
+    circle.style.background = color
+}
+
+// menu hamburguinho
+function mudouTamanho() {
+    if (window.innerWidth >= 700) {
+        itens_menu.style.display = 'block'
+    } else {
+        itens_menu.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itens_menu.style.display == 'block') {
+    itens_menu.style.display = 'none'
+   } else {
+    itens_menu.style.display = 'block'
+   }
+}
