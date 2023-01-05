b84e74899b44a026dfbeb69b72c067970c20e0bf
Luiz Paulo Terra Nogueira <119353957+gueroso1@users.noreply.github.com>
2023-01-05 17:22:49 -0300

    site mobile

---
+++ the-boys/script.js
@@ -0,0 +1,16 @@
+// menu hamburguinho
+function mudouTamanho() {
+    if (window.innerWidth>=769) {
+        itens_menu.style.display = 'block'
+    } else {
+        itens_menu.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (itens_menu.style.display == 'block') {
+    itens_menu.style.display = 'none'
+   } else {
+    itens_menu.style.display = 'block'
+   }
+}
